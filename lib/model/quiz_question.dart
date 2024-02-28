cf7992d7965a842698301597d27c42620a59ca40
Mohamed Dawood <mhddawood786@gmail.com>
2024-02-28 15:42:05 +0530

    first commit

---
+++ lib/model/quiz_question.dart
@@ -0,0 +1,5 @@
+class QuizQuestion {
+  const QuizQuestion(this.question, this.answer);
+  final String question;
+  final List<String> answer;
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = "start-screen";
+
+  /* @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }*/
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = "question-screen";
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          //child: activeScreen,
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
